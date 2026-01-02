a07ed70c702ffceebadd40f08ca571bc04fbb9f3
Jaynish Shah <10665032+jsha129@users.noreply.github.com>
2026-01-02 17:54:20 +1100

    Add os module import to Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
 # ---- File: Home.py ----
 import streamlit as st
 import pandas as pd
+import os
 
 
 @st.cache_data
@@ -12,6 +13,7 @@ def load_data(uploaded_file, sheet):
 st.set_page_config(page_title="Stock Fundamental Data Viewer")
 st.session_state.fetchLiveData = st.checkbox('Fetch live data?', value = True)
 
+os.listdir(os.getcwd())
 uploaded_file = 'data/input.xlsx'
 st.session_state.df = load_data(uploaded_file, "asx_index_filtered")
 st.session_state.df_cagr = load_data(uploaded_file, "CAGR")
