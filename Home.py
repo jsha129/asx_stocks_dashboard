34789405fcb8a0f564884c46d198d454a97f2e49
Jaynish Shah <10665032+jsha129@users.noreply.github.com>
2026-01-11 23:41:30 +1100

    Refactor Home.py for file upload and data handling
    Asks a user to upload file

--- Home.py
+++ Home.py
@@ -1,7 +1,6 @@
-# ---- File: Home.py ----
+# ---- File: app.py ----
 import streamlit as st
 import pandas as pd
-import os
 
 
 @st.cache_data
@@ -11,26 +10,27 @@ def load_data(uploaded_file, sheet):
 
 
 st.set_page_config(page_title="Stock Fundamental Data Viewer")
-st.session_state.fetchLiveData = st.checkbox('Fetch live data?', value = True)
 
-# os.listdir(os.getcwd())
-uploaded_file = 'data/input.xlsx'
-st.session_state.df = load_data(uploaded_file, "asx_index_filtered")
-st.session_state.df_cagr = load_data(uploaded_file, "CAGR")
-temp_avg_fund = load_data(uploaded_file, 'avg_fundamentals')
-temp_avg_fund = temp_avg_fund.set_index('Code')
-temp_avg_fund.index.name = None 
-st.session_state.df_avg_fund = temp_avg_fund
-#-- 
-temp_raw_fund = load_data(uploaded_file, 'raw_fundamentals')
-temp_raw_fund['Code'] = temp_raw_fund['Code'].apply(lambda x: x.split('.')[0]) 
-temp_raw_fund = temp_raw_fund.set_index('Code')
-temp_raw_fund.index.name = None 
-temp_raw_fund = temp_raw_fund.rename( columns = {'Unnamed: 0': 'Matric'})
-st.session_state.df_raw_fund = temp_raw_fund
-##--
-st.success('Data uploaded successfully!')
-if st.button("Let's start"):
-    st.session_state.stocks = ['CBA']
-    st.switch_page("pages/Stocks_Dashboard.py")
 
+st.title("Upload File")
+uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx", "xls"])
+
+
+if uploaded_file:
+    st.session_state.df = load_data(uploaded_file, "asx_index_filtered")
+    st.session_state.df_cagr = load_data(uploaded_file, "CAGR")
+    temp_avg_fund = load_data(uploaded_file, 'avg_fundamentals')
+    temp_avg_fund = temp_avg_fund.set_index('Code')
+    temp_avg_fund.index.name = None 
+    st.session_state.df_avg_fund = temp_avg_fund
+    #-- 
+    temp_raw_fund = load_data(uploaded_file, 'raw_fundamentals')
+    temp_raw_fund['Code'] = temp_raw_fund['Code'].apply(lambda x: x.split('.')[0]) 
+    temp_raw_fund = temp_raw_fund.set_index('Code')
+    temp_raw_fund.index.name = None 
+    temp_raw_fund = temp_raw_fund.rename( columns = {'Unnamed: 0': 'Matric'})
+    st.session_state.df_raw_fund = temp_raw_fund
+    ##--
+    # st.session_state.stocks = ['CBA']
+    st.session_state.stocks =[]
+    st.switch_page("pages/01_Dashboard.py")
